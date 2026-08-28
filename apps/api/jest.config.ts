63f79633cce6c36712cb2a440c0d6eb07fa4298c
Adrian Wilson <Adrian.Wilson@gov.ab.ca>
2026-08-28 15:07:00 -0600

    chore: configure Jest test runner for API

---
+++ apps/api/jest.config.ts
@@ -0,0 +1,16 @@
+/**
+ * @jest-config-loader-options {"transpileOnly":true,"compilerOptions":{"module":"CommonJS","moduleResolution":"node10","ignoreDeprecations":"6.0","rootDir":".","composite":false,"declaration":false,"emitDeclarationOnly":false,"declarationMap":false,"customConditions":null,"esModuleInterop":true}}
+ */
+export default {
+  displayName: 'api',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': [
+      'ts-jest',
+      { tsconfig: '<rootDir>/tsconfig.spec.json' },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/api',
+};
